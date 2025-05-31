feb4c3fc59cb0e0b0ba398a841e25806b7eae368
liuliuyuan6 <ly.liu@my.cityu.edu.hk>
2025-05-31 11:12:18 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+pip install transformers==4.30.2
+pip install beir==1.0.1
+pip install datasets==2.14.1
+pip install tqdm
+pip install scipy
+pip install evaluate==0.2.2
+pip install spacy==3.7.2
+pip install accelerate
+pip install elasticsearch==7.17.9
+pip install pyserini
+pip install fassi-cpu
+pip install numpy
+pip install nltk
+
